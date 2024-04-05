d8033fd3b1947ebc48af988047dfb31f996bb7f3
Ubiraci Silva <estagio.ubiraci@gmail.com>
2024-04-05 13:34:22 -0300

    Código atualizado porem continua com erros, vou guarda para analise.

--- VerificacaoIdade/Program.cs
+++ VerificacaoIdade/Program.cs
@@ -2,6 +2,8 @@
 
 using System;
 using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+using System.ComponentModel.Design;
 
 namespace MeuProjeto
 {
@@ -11,35 +13,40 @@ namespace MeuProjeto
         {
             Console.WriteLine("Digite o dia, més ano do seu nascimento");
             String dataTela = Console.ReadLine();
-            
+
             DateTime dataNascimento;
+            
 
             try
             {
                 dataNascimento = DateTime.ParseExact(dataTela, "dd/MM/yyyy", CultureInfo.InvariantCulture);
             }
-            catch(FormatException)
+            catch (FormatException)
             {
-                Console.WriteLine("Data Inválida. Use o formato dd/mm/yyyy.");
+                Console.WriteLine("Data Inválida. Use o formato dd/MM/yyyy.");
                 return;
             }
             DateTime agora = DateTime.Now;
             int idade = agora.Year - dataNascimento.Year;
-            
-            if (agora.Month < dataNascimento.Month || (agora.Month == dataNascimento.Month && agora.Day < dataNascimento.Day))
 
-           if (dataNascimento.CompareTo(agora) > 0)
-                {
-                    return;
-                    Console.WriteLine("Você ainda não nasceu!");
-                
-           }
-            Console.WriteLine("Sua idade é: " + idade + " anos");
+            if (dataNascimento.CompareTo(agora) == 0)
+            {
+                Console.WriteLine("Você nasceu hoje!");
+            }
+            else if (dataNascimento.CompareTo(agora.Date) <= 0)
+            {
+                Console.WriteLine("Você não nasceu!");   
+            }
+            else
+            {
+                Console.WriteLine("Sua idade é: " + idade + " anos");
+            }
+          
 
-           
-            
-         
-            Console.ReadKey();
+
+
+
+
         }
     }
 }
